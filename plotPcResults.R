819987d2e28d8e7ff1f6136b0f6379082aee34cf
Marcin.Niesluchowski <Marcin.Niesluchowski@gmail.com>
2013-01-08 22:31:41 +0000

    Rezultaty i dystrybuanty dla wszystkich pc w jednym pliku oraz plik z rysowaniem dystrybuant

--- plotPcResults.R
+++ plotPcResults.R
@@ -1,3 +1,6 @@
 plot(0,0, xlim=c(500,1500), ylim=c(0.0, 1.0), type="n", main="Dystrybuanty", xlab="wartość funkcji celu", ylab="prawdopodobieństwo")
-lines(p1pc10resultsDistribution, col="blue")
-lines(p1pc00resultsDistribution, col="red")
+lines(p1pc00resultsDistribution, col="blue")
+lines(p1pc02resultsDistribution, col="violet")
+lines(p1pc04resultsDistribution, col="red")
+lines(p1pc07resultsDistribution, col="yellow")
+lines(p1pc10resultsDistribution, col="green")
