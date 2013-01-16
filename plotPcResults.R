9a79237af9dc5c7abfc81b1df242f7ddaba63e4e
KasiaTar <KasiaTar@gmail.com>
2013-01-16 14:11:26 +0000

    Sortowanie: Dystrybuanty dla 20 uruchomien s=3

--- plotPcResults.R
+++ plotPcResults.R
@@ -1,3 +1,6 @@
+#PROBLEM SORTOWANIA
+
+#parametr turnieju s=2
 plot(0,0, xlim=c(500,1500), ylim=c(0.0, 1.0), type="n", main="Dystrybuanty", xlab="wartość funkcji celu", ylab="prawdopodobieństwo")
 lines(p1pc00resultsDistribution, col="blue")
 lines(p1pc02resultsDistribution, col="violet")
@@ -5,7 +8,15 @@ lines(p1pc02resultsDistribution, col="violet")
 lines(p1pc07resultsDistribution, col="yellow")
 lines(p1pc10resultsDistribution, col="green")
 
+#parametr turnieju s=3
+plot(0,0, xlim=c(500,800), ylim=c(0.0, 1.0), type="n", main="Dystrybuanty dla parametru turnieju s=3", xlab="wartość funkcji celu dla problemu sortowania", ylab="prawdopodobieństwo")
+lines(p1t3pc00Distribution, col="blue")
+lines(p1t3pc02Distribution, col="violet")
+lines(p1t3pc04Distribution, col="red")
+lines(p1t3pc07Distribution, col="yellow")
+lines(p1t3pc10Distribution, col="green")
 
+#parametr turnieju s=5
 plot(0,0, xlim=c(300,700), ylim=c(0.0, 1.0), type="n", main="Dystrybuanty dla parametru turnieju s=5", xlab="wartość funkcji celu", ylab="prawdopodobieństwo")
 lines(p1s5pc00resultsDistribution, col="blue")
 lines(p1s5pc02resultsDistribution, col="violet")
@@ -14,7 +25,7 @@ lines(p1s5pc07resultsDistribution, col="yellow")
 lines(p1s5pc10resultsDistribution, col="green")
 
 
-
+#PROBLEM PLECAKOWY
 plot(0,0, xlim=c(220,290), ylim=c(0.0, 1.0), type="n", main="Dystrybuanty parametru turnieju s=2", xlab="wartość funkcji celu dla problemu plecakowego", ylab="prawdopodobieństwo")
 lines(prt2pc00Distribution, col="blue")
 lines(prt2pc02Distribution, col="violet")
