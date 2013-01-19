7e1f7ffd933f32646e476334e2108d0e9364744e
Marcin.Niesluchowski <Marcin.Niesluchowski@gmail.com>
2013-01-19 15:44:19 +0000

    Poprawiona ewaluacja

--- evaluationRsc.R
+++ evaluationRsc.R
@@ -3,12 +3,14 @@ evaluationTableRsk<-function(coordinates,profity, wagi)
   wSum <- 0
   pSum <- 0
   i <-  1
-  while((wSum <= 100)){
+  while(TRUE){
     #|| (i>=(length(coordinates)))){
     ind <- coordinates[[i]]
     wSum <- wSum + wagi[[1]][[ind]]
+    if((wSum > 100)){
+      return (pSum)
+    }
     pSum <- pSum + profity [[1]][[ind]]
     i<-i+1
   }
-  return (pSum)
 }
